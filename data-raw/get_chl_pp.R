0e0f8de2c511e1c9d24216d768b1d0bbd0ca1573
Kimberly Bastille <39955661+kimberly-bastille@users.noreply.github.com>
2020-01-09 09:07:13 -0500

    character return

--- data-raw/get_chl_pp.R
+++ data-raw/get_chl_pp.R
@@ -7,25 +7,35 @@ library(stringr)
 raw.dir <- here::here("data-raw")
 
 
-ppd <- read.csv(file.path(raw.dir,"SOE_V2019_2-NES_ECOREGIONS-PPD-STATS_ANOMS-SEAWIFS_MODIS.csv")) %>% 
+ppd <- read.csv(file.path(raw.dir,
+                          "SOE_V2019_2-NES_ECOREGIONS-PPD-STATS_ANOMS-SEAWIFS_MODIS.csv")) %>% 
   mutate(ALGORITHM = word(str_replace(ALGORITHM, "_", " "))) %>%
   unite(.,VARIABLE, c("VARIABLE","SENSOR","ALGORITHM"), sep = " ") %>%
-  mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), paste(VARIABLE,"1998_2018"),
-                           ifelse(str_detect(FILENAME, "1998_2017"), paste(VARIABLE, "1998_2017"),
-                                  ifelse(str_detect(FILENAME, "1997_2018"), paste(VARIABLE, "1997_2018"),
-                                         ifelse(str_detect(FILENAME, "1997_2017"), paste(VARIABLE, "1997_2017"),
+  mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), 
+                           paste(VARIABLE,"1998_2018"),
+                           ifelse(str_detect(FILENAME, "1998_2017"), 
+                                  paste(VARIABLE, "1998_2017"),
+                                  ifelse(str_detect(FILENAME, "1997_2018"), 
+                                         paste(VARIABLE, "1997_2018"),
+                                         ifelse(str_detect(FILENAME, "1997_2017"), 
+                                                paste(VARIABLE, "1997_2017"),
                                                 VARIABLE))))) %>%
   dplyr::select(TIME, UNITS, VARIABLE, VALUE, REGION) %>%
   dplyr::rename(Time = TIME, Units = UNITS, Var = VARIABLE,
                 EPU = REGION, Value = VALUE)
 
-chl <- read.csv(file.path(raw.dir,"SOE_V2019_2-NES_ECOREGIONS-CHLOR_A-STATS_ANOMS-SEAWIFS_MODIS.csv")) %>% 
+chl <- read.csv(file.path(raw.dir,
+                          "SOE_V2019_2-NES_ECOREGIONS-CHLOR_A-STATS_ANOMS-SEAWIFS_MODIS.csv")) %>% 
   mutate(ALGORITHM = word(str_replace(ALGORITHM, "_", " "))) %>%
   unite(.,VARIABLE, c("VARIABLE","SENSOR","ALGORITHM"), sep = " ") %>%
-  mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), paste(VARIABLE,"1998_2018"),
-                           ifelse(str_detect(FILENAME, "1998_2017"), paste(VARIABLE, "1998_2017"),
-                                  ifelse(str_detect(FILENAME, "1997_2018"), paste(VARIABLE, "1997_2018"),
-                                         ifelse(str_detect(FILENAME, "1997_2017"), paste(VARIABLE, "1997_2017"),
+  mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), 
+                           paste(VARIABLE,"1998_2018"),
+                           ifelse(str_detect(FILENAME, "1998_2017"), 
+                                  paste(VARIABLE, "1998_2017"),
+                                  ifelse(str_detect(FILENAME, "1997_2018"), 
+                                         paste(VARIABLE, "1997_2018"),
+                                         ifelse(str_detect(FILENAME, "1997_2017"), 
+                                                paste(VARIABLE, "1997_2017"),
                                                 VARIABLE))))) %>%
   dplyr::select(TIME, UNITS, VARIABLE, VALUE, REGION) %>%
   dplyr::rename(Time = TIME, Units = UNITS, Var = VARIABLE,
